8e36226f0b51a9ac2647ebf0ab2bbbe4ed7f7726
lokeslokesh12 <lokesh272203@gmail.com>
2022-10-05 13:10:03 +0530

    onload

---
+++ javascript/onload.js
@@ -0,0 +1,8 @@
+function onload(){
+    document.getElementById("onload-fram").style.display = 'flex' ;
+    document.getElementById("header").style.position =''
+}
+function close_button(){
+    document.getElementById("onload-fram").style.display = 'none' ;
+
+}
