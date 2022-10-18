0d200a2ee15827ce058faf33a648cb9013467b75
lokeslokesh12 <lokesh272203@gmail.com>
2022-10-18 19:49:38 +0530

    datk-mode

--- javascript/onload.js
+++ javascript/onload.js
@@ -1,10 +1,26 @@
-function onload(){
-    document.getElementById("onload-fram").style.display = 'flex' ;
-    document.body.style.overflow = 'hidden'
+// function onload(){
+//     document.getElementById("onload-fram").style.display = 'flex' ;
+//     document.body.style.overflow = 'hidden'
    
-}
-function close_button(){
-    document.getElementById("onload-fram").style.display = 'none' ;
-    document.body.style.overflow = 'auto'
+// }
+// function close_button(){
+//     document.getElementById("onload-fram").style.display = 'none' ;
+//     document.body.style.overflow = 'auto'
 
+// }
+
+// for change darkmode -- lightmode
+
+
+function mode(){
+    let img = document.getElementById("image-mode");
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+        document.getElementById("mode-p").innerText = 'LIGHT'
+        img.src="./images/sun.png";
+    }
+    else{
+        document.getElementById("mode-p").innerText = 'DARK' 
+        img.src="./images/moon.png"
+    }
 }
